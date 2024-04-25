1a18ad0799b7122e177d32209d8adac0bcb257ef
Rodolfo Gutierrez <rag.ortega@gmail.com>
2024-04-25 16:28:01 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rodolfo Gutierrez on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
